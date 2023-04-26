2cd7ca2183e95cfe96c80103e3032c95dd182455
minyoung <aroem0825@naver.com>
2023-04-26 15:47:20 +0900

    .

---
+++ src/App.vue
@@ -0,0 +1,64 @@
+<template>
+  <div>
+    <h1>KANBAN Borad</h1>
+  </div>
+  <div class="todo">
+    <div class="todoList">
+      <p>TodoList</p>
+      <div class="inputBox">
+        todo : <input type="text" placeholder="오늘 할일은 무엇입니까?" @keyup.enter="addTodo"/>
+      </div>
+      <ul>
+        <li v-for="(todo, index) in todos" :key="index">
+          <input type="checkBox"/>{{ todo }}
+        </li>
+      </ul>
+    </div>
+    <PostCreate @create="createPost"/>
+  </div>
+</template>
+
+<script setup>
+import {  reactive,ref } from 'vue'
+import PostCreate from './components/PostCreate.vue'
+
+
+const todos = reactive([]);
+const addTodo = event => {
+	todos.push(event.target.value);
+
+	console.log(`저장되었습니다. ${todos}`);
+};
+
+const createPost =()=>{
+  console.log("rr")
+}
+
+</script>
+
+<style lang="scss" scoped>
+.todo {
+  display: grid;
+  grid-template-columns: 30% 30% 30%;
+  gap: 10%;
+
+  .todoList {
+    background-color: red;
+    height: 100%;
+
+    p {
+      color: #fff;
+    }
+    ul{
+      list-style: none;
+      padding: 0;
+      li{
+        margin: 0;
+        background-color: #fff;
+        padding: 20px;
+        margin: 30px;
+      }
+    }
+  }
+}
+</style>
---
+++ src/components/PostCreate.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <p>PostComponet</p>
+    <button @click="create">button</button>
+  </div>
+</template>
+
+<script setup>
+
+
+const create = ()=>{
+console.log("tt")
+}
+</script>
+
+<style lang="scss" scoped></style>
